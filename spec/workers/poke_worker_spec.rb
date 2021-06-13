ffe27fc23e07daa2fc6e7410c6989e18aed6caeb
berfail <kunsangaboberfy@gmail.com>
2021-06-13 23:54:37 +0200

    add worker spec

--- spec/workers/poke_worker_spec.rb
+++ spec/workers/poke_worker_spec.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+Sidekiq::Testing.fake!
+
 RSpec.describe PokeWorker, type: :worker do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:worker) { described_class.new }
+
+  describe "#perform_async" do
+    it "queues poke worker job" do
+      expect { PokeWorker.perform_async }.to change(PokeWorker.jobs, :size).by(1)
+    end
+  end
+
+  describe "#perform" do
+    it "should respond to #perform" do
+      expect(worker).to respond_to(:perform)
+    end
+  end
 end
