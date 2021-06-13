7baa54ed883724e4b4b22e85a60c16b26d963719
berfail <kunsangaboberfy@gmail.com>
2021-06-14 00:15:54 +0200

    worker spec done

--- spec/workers/poke_worker_spec.rb
+++ spec/workers/poke_worker_spec.rb
@@ -1,20 +1,30 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'spec_helper'
 Sidekiq::Testing.fake!
 
 RSpec.describe PokeWorker, type: :worker do
   subject(:worker) { described_class.new }
 
-  describe "#perform_async" do
-    it "queues poke worker job" do
+  describe '#perform_async' do
+    it 'queues poke worker job' do
       expect { PokeWorker.perform_async }.to change(PokeWorker.jobs, :size).by(1)
     end
   end
 
-  describe "#perform" do
-    it "should respond to #perform" do
+  describe '#perform' do
+    before do
+      stub_request(:get, 'https://pokeapi.co/api/v2/pokemon/1')
+        .to_return(status: 200, body: '', headers: {})
+    end
+    it 'should respond to #perform' do
       expect(worker).to respond_to(:perform)
+    end
+    it 'enqueues another poke worker job' do
+      expect_any_instance_of(PokeService).to receive(:call).once
+
+      worker.perform
     end
   end
 end
