4233389cbab753bb7d4c494cebfff062a7a2ab5d
berfail <kunsangaboberfy@gmail.com>
2021-06-14 00:54:02 +0200

    add rails env condition

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,4 +1,4 @@
 
 Swagger::Docs::Config.register_apis({
-  "1.0" => {:controller_base_path => "", :api_file_path => "public"}
+  "1.0" => {:controller_base_path => "", :api_file_path => "public/"}
 })
